2f1ac53ba03a41510e4ed4fef67ccd7bc34cb7b2
ESET0104 <lakshmiprasadk20@gmail.com>
2025-09-12 13:16:04 +0530

    updated assignment 2

--- MSSQL/Assignment_2.sql
+++ MSSQL/Assignment_2.sql
@@ -5,13 +5,15 @@ select * from orders
 select * from books
 
 -- Task1: Top N books per user
+select * from (
 select b.users_id, c.names, a.title,
 ROW_NUMBER() over(partition by b.users_id order by a.price desc) as expense,
 rank() over(partition by b.users_id order by a.price desc) as expense_rank,
 dense_rank() over(partition by b.users_id order by price desc) as d_rank
 from books a inner join orders b on a.product_id = b.product_id
 inner join users c on b.users_id = c.users_id
-where expense <= 2 order by b.users_id, expense
+) as rankedbooks
+where expense <= 2 order by users_id, expense
 
 -- Task2: Ranking users by total spending
 select b.users_id, sum(price) as total,
